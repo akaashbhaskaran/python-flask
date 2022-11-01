ba372a1e5010875a210ecee5c9506d3cad412b65
Akash <akash@Akashs-MacBook-Air.local>
2022-11-02 04:42:41 +0530

    Added retry and exception handling

--- app.py
+++ app.py
@@ -6,25 +6,34 @@ def create_app():
 
 	@app.route('/')
 	def sort():
+		try:
+			endpoints = ['https://raw.githubusercontent.com/assignment132/assignment/main/duckduckgo.json','https://raw.githubusercontent.com/assignment132/assignment/main/google.json','https://raw.githubusercontent.com/assignment132/assignment/main/wikipedia.json']
 
-		endpoints = ['https://raw.githubusercontent.com/assignment132/assignment/main/duckduckgo.json','https://raw.githubusercontent.com/assignment132/assignment/main/google.json','https://raw.githubusercontent.com/assignment132/assignment/main/wikipedia.json']
-	
-		if 'sortKey' not in request.args or 'limit' not in request.args:
-			return 'Input params missing' , 400 
-		sortKey = request.args.get('sortKey')
-		limit = int(request.args.get('limit'))
-		data = []
-		for url in endpoints:
-			r = requests.get(url)
-		
-			if r.status_code == 200:
-				data.extend(r.json()['data'])
-		
-		if sortKey not in data[0]:
-			return 'Invalid Sort Key', 400
-		data = sorted(data, key=lambda item: item[sortKey], reverse=False)
-	
-		return {'data':data[0:limit],'count':limit}
+			if 'sortKey' not in request.args or 'limit' not in request.args:
+				return 'Input params missing' , 400
+			sortKey = request.args.get('sortKey')
+			limit = int(request.args.get('limit'))
+			data = []
+			MAX_RETRIES = 3
+			for url in endpoints:
+
+				for i in range(MAX_RETRIES):
+					r = requests.get(url)
+
+					if r.status_code == 200:
+						data.extend(r.json()['data'])
+						break
+					else:
+						print("Attempting retry {} for {}".format(i,url))
+						continue
+
+			if sortKey not in data[0]:
+				return 'Invalid Sort Key', 400
+			data = sorted(data, key=lambda item: item[sortKey], reverse=False)
+			data = data[0:limit]
+			return {'data':data,'count':len(data)}
+		except:
+			return 'Internal Server error', 500
 
 	return app
 # main driver function
